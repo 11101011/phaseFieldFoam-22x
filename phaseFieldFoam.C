2a47af33016c3283c575b916ba7c86c4123e7345
SebastianWeiss <nero235@openmailbox.org>
2017-06-13 09:57:26 +0200

    modified output boundary min

--- phaseFieldFoam.C
+++ phaseFieldFoam.C
@@ -69,6 +69,13 @@ int main(int argc, char *argv[])
     bool t = true;
     bool b = true;
 
+    //-Obtain a list of all boundaries on the mesh
+    const fvPatchList& patches = mesh.boundary();
+    int N = static_cast<int> (patches.size());
+
+    scalar boundaryMin[N];
+    bool boundaryMin_t[N];
+
     Info<< "\nStarting time loop\n" << endl;
 
     while (runTime.run())
